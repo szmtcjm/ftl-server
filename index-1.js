cffbf73c71cfb3528588eb31b2b4c932cfb9ce66
陈江明 <hzchenjiangming@corp.netease.com>
2015-03-07 16:10:47 +0800

    'init'

---
+++ index-1.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var logger = require('morgan');
+
+var routes = require('./routes/index');
+var config = require('./lib/config');
+var ftl = require('./lib/ftl');
+var proxy = require('./lib/proxy');
+var mock = require('./lib/mock');
+var list = require('./lib/list');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', list);
+app.use(ftl);
+app.use(proxy);
+app.use(mock);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//如果是ajax错误，返回json信息
+function xhrErrorHandler(err, req, res, next) {
+    res.status(err.status || 500);
+  if (req.xhr) {
+    res.send({
+        message: err.message,
+        error: err
+    });
+  } else {
+    next(err);
+  }
+}
+
+// 其他错误, 404的话返回404页面
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+module.exports = app;
