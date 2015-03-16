f7d09d2af608e6690c6067c83e2c9af224b7805f
陈江明 <hzchenjiangming@corp.netease.com>
2015-03-16 15:28:25 +0800

    0.1.1

---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var argv = require('../lib/argv');
+var fs = require('fs');
+var fork = require('child_process').fork;
+var path = require('path');
+
+var configPath = path.resolve(argv.c);
+
+var child = forkApp();
+
+fs.watchFile(configPath, function(curr, prev) {
+    if (curr.mtime !== prev.mtine) {
+        child && child.kill();
+    }
+})
+
+function forkApp() {
+    console.log('the ftl server starting');
+    child = fork(path.join(__dirname, '..', 'app'), process.argv.slice(2));
+    child.on('exit', function() {
+        child = forkApp();
+    });
+    return child;
+}
